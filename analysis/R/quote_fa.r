6342dd1f0573e248eeca1f2bd6f2f4d66ebdef52
gww <269164548@qq.com>
2016-03-03 07:06:36 +0000

    123

--- analysis/R/quote_fa.r
+++ analysis/R/quote_fa.r
@@ -2,20 +2,26 @@
 
 library(RSQLite)
 
-conn <- dbConnect(SQLite(), "e:/gww/MIT/stock/sqlite_db/extra_stocks.db")
-query<-dbSendQuery(conn, "select * from '603077_extra'")
+conn <- dbConnect(SQLite(), "../../sqlite_db/extra_stocks.db")
+query<-dbSendQuery(conn, "select * from '300231_extra'")
 quote<-fetch(query, n=-1)
 quote
 
-d1<-with(quote,  {
-	pr<-price
-	r1<-rratio1
-	r5<-rratio5
-	r10<-rratio10
-	r20<-ratio20
-	r30<-rratio30
-	r60<-rratio60
-	v1<-volume_ratio
+d1<-with(quote,  {data.frame(
+	pr=price,
+	r1=rratio1,
+	r5=rratio5,
+	r10=rratio10,
+	r20=rratio20,
+	r30=rratio30,
+	r60=rratio60,
+	v1=volume_ratio,
+	vr5=volume_ratio_5,
+	vr10=volume_ratio_10,
+	vr20=volume_ratio_20,
+	vr30=volume_ratio_30,
+	vr60=volume_ratio_60,
+	amp=amplitudes)
 	})
 
-
+factanal(x = d1, factors = 5, ratation = "promax")->ft
